0fa528189e636b7aba4d13e86e49558f3d74cc66
guinan.lin <guinan.lin@foxmail.com>
2025-10-15 17:30:21 +0800

    chore(cli): release v0.1.37

--- packages/components/dty-lucky-sheet/typings/ssf.d.ts
+++ packages/components/dty-lucky-sheet/typings/ssf.d.ts
@@ -1,4 +1,21 @@
+// 为不同的相对路径导入提供类型声明
 declare module '../core/modules/ssf' {
+  const SSF: {
+    format: (fmt: string, value: any) => string;
+    is_date: (fmt: string, value?: any) => boolean;
+  };
+  export default SSF;
+}
+
+declare module './ssf' {
+  const SSF: {
+    format: (fmt: string, value: any) => string;
+    is_date: (fmt: string, value?: any) => boolean;
+  };
+  export default SSF;
+}
+
+declare module '../modules/ssf' {
   const SSF: {
     format: (fmt: string, value: any) => string;
     is_date: (fmt: string, value?: any) => boolean;
